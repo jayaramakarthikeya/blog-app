3f2dddfd32e947e0d1ac890595f3f491e33c626d
JayaramKarthik <karthikeyaj82@gmail.com>
2020-12-31 12:15:32 +0530

    update and deleting posts

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,57 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from .models import Post
 
-posts = [
-	 {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
-# Create your views here.
+
 def home(request):
 	context = {
 		'posts':Post.objects.all()
 	}
 	return render(request,'blog/home.html',context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+    	form.instance.author = self.request.user
+    	return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+    	form.instance.author = self.request.user
+    	return super().form_valid(form)
+
+    def test_func(self):
+    	post = self.get_object()
+    	if self.request.user == post.author:
+    		return True
+    	return False
+
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+    	post = self.get_object()
+    	if self.request.user == post.author:
+    		return True
+    	return False
 
 def about(request):
 	return render(request,'blog/about.html',{'title':'About'})
